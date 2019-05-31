c5d43bec648ddcc8c8429d26e7ceac0e86b09fb9
georgesotirov <gsotirovapm@gmail.com>
2019-05-31 03:46:43 +0300

    Added Menu
    Need to add button functionality and animations.
    
    Re-create the menu in After Effects and animate different elements.

---
+++ Assets/Scripts/Start_Show_Particle_System.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Show_Particle_System : MonoBehaviour
+
+    {
+    public GameObject particlesystem;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particlesystem.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
